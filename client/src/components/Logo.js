30737a8f682eec8345b95ebc6e259797fb2c2418
Mike Goforth <mike.goforth@outlook.com>
2023-05-17 08:45:16 -0500

    init client logic

---
+++ client/src/components/Logo.js
@@ -0,0 +1,5 @@
+const Logo = () => {
+  return <span className='logo'>AppTrak</span>
+}
+
+export default Logo
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,41 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
+import React from 'react';
+
+const Landing = () => {
+  const { user } = useAppContext();
+  return (
+    <>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              Great! Your application has been sent. Now what? <br />
+              <br />
+              <span>AppTrak</span> is a simple app that helps you keep track of
+              your job applications. <br />
+            </p>
+            <Link to='/register' className='btn btn-hero'>
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Landing;
